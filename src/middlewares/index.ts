7f1d36aac2ecf304a872f50a22f62a3a4cbdd428
hgiudatto <hgiudatto@prismamp.com>
2023-04-30 18:52:26 -0300

    user update - delete

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,8 +1,37 @@
 import express from "express";
-import { get, merge } from "lodash";
-import { getUserBySessionToken } from "../db/users";
+import pkg from "lodash";
+const { merge, get } = pkg;
+// import { get, merge } from "lodash";
+import { getUserBySessionToken } from "../db/users.js";
 
-export const isAuthntecated = async (
+export const isOwner = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  try {
+    const { id } = req.params;
+    const currentUserId = get(
+      req,
+      `identity._id`
+    ) as unknown as string;
+
+    if (!currentUserId) {
+      return res.sendStatus(403);
+    }
+
+    if (currentUserId.toString() !== id) {
+      return res.sendStatus(403);
+    }
+
+    next();
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(400);
+  }
+};
+
+export const isAuthenticated = async (
   req: express.Request,
   res: express.Response,
   next: express.NextFunction
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import authentication from "./authentication.js";
+import users from "./users.js";
 
 const router = express.Router();
 
 export default (): express.Router => {
   authentication(router);
+  users(router);
 
   return router;
 };
